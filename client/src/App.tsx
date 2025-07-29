75b0d7499cb8895eb676abdebc34760d2337c0fd
Jesus Sunuc <mariano.code4guate@gmail.com>
2025-07-28 23:39:27 -0600

    clean up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <button className="btn btn-primary">Click Me</button>
+      <div className="container">
+        <h1>Hola</h1>
+      </div>
     </>
   );
 }
